ea4aca5241abed1eebb3245e2af936d3cdd4e3ba
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-05-17 16:28:55 +0300

    Adding the Line structure and declaring the main functions for working with it

--- TheFirstAssignment/text_editor.h
+++ TheFirstAssignment/text_editor.h
@@ -1,7 +1,19 @@
 #ifndef THEFIRSTASSIGNMENT_TEXT_EDITOR_H
 #define THEFIRSTASSIGNMENT_TEXT_EDITOR_H
+
 typedef struct Line {
     char *text;
     struct Line *next;
 } Line;
+
+void removeNewline(char *str);
+void appendText(Line **head, const char *text);
+void startNewLine(Line **head);
+void saveToFile(Line *head, const char *filename);
+void loadFromFile(Line **head, const char *filename);
+void printCurrentText(Line *head);
+void insertText(Line *head, int lineNumber, int charIndex, const char *text);
+void searchText(Line *head, const char *query);
+void clearText(Line **head);
+
 #endif //THEFIRSTASSIGNMENT_TEXT_EDITOR_H
