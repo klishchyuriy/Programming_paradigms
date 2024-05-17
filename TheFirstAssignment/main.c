880481988c0f8abd63ff8d97e77c1a5fd3662890
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-05-17 18:36:44 +0300

    Implement main function with menu for text editor

--- TheFirstAssignment/main.c
+++ TheFirstAssignment/main.c
@@ -33,6 +33,65 @@ int main() {
                 appendText(&head, text);
             }
                 break;
+            case 2:
+                startNewLine(&head);
+                printf("New line is started\n");
+                break;
+            case 3:
+            {
+                char filename[1024];
+                printf("Enter the file name for saving: ");
+                fgets(filename, sizeof(filename), stdin);
+                removeNewline(filename);
+                saveToFile(head, filename);
+                printf("Text has been saved successfully\n");
+            }
+                break;
+            case 4:
+            {
+                char filename[1024];
+                printf("Enter the file name for loading: ");
+                fgets(filename, sizeof(filename), stdin);
+                removeNewline(filename);
+                loadFromFile(&head, filename);
+                printf("Text has been loaded successfully\n");
+            }
+                break;
+            case 5:
+                printCurrentText(head);
+                break;
+            case 6:
+            {
+                int lineNumber, charIndex;
+                char text[1024];
+                printf("Enter line number and character index: ");
+                scanf("%d %d", &lineNumber, &charIndex);
+                getchar();
+                printf("Enter text to insert: ");
+                fgets(text, sizeof(text), stdin);
+                removeNewline(text);
+                insertText(head, lineNumber, charIndex, text);
+            }
+                break;
+            case 7:
+            {
+                char query[1024];
+                printf("Enter text to search: ");
+                fgets(query, sizeof(query), stdin);
+                removeNewline(query);
+                searchText(head, query);
+            }
+                break;
+            case 8:
+                printf("\033[2J\033[1;1H");
+                break;
+            case 0:
+                clearText(&head);
+                printf("Exiting the editor...\n");
+                return 0;
+            default:
+                printf("Invalid command, please try again.\n");
+                break;
         }
     }
 }
