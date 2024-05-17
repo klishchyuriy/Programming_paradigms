01971652f32b0782253a056006baf44e6e37fd21
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-05-17 18:35:21 +0300

    Implement main function with menu (case 1) for text editor

--- TheFirstAssignment/main.c
+++ TheFirstAssignment/main.c
@@ -2,7 +2,37 @@
 #include <string.h>
 #include "text_editor.h"
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+void printMenu() {
+    printf("Choose the command:\n");
+    printf("1. Append text symbols to the end\n");
+    printf("2. Start a new line\n");
+    printf("3. Save to file\n");
+    printf("4. Load from file\n");
+    printf("5. Print current text\n");
+    printf("6. Insert text by line and symbol index\n");
+    printf("7. Search text\n");
+    printf("8. Clear console\n");
+    printf("0. Exit\n");
+}
+
+int main() {
+    Line *head = NULL;
+    int command;
+    while (1) {
+        printMenu();
+        printf("> ");
+        scanf("%d", &command);
+        getchar();
+
+        switch (command) {
+            case 1: {
+                char text[1024];
+                printf("Enter text to append: ");
+                fgets(text, sizeof(text), stdin);
+                removeNewline(text);
+                appendText(&head, text);
+            }
+                break;
+        }
+    }
 }
