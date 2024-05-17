82e8c8cbd8d851454feff49388de449cc9034d89
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-05-17 16:34:59 +0300

    Implement removeNewline and startNewLine functions

--- TheFirstAssignment/text_editor.c
+++ TheFirstAssignment/text_editor.c
@@ -1,4 +1,28 @@
-//
-// Created by Yuriy Klishch on 17.05.2024.
-//
+#include <stdlib.h>
+#include <string.h>
 #include "text_editor.h"
+
+
+void removeNewline(char *str) {
+    size_t length = strlen(str);
+    if (length > 0 && str[length - 1] == '\n') {
+        str[length - 1] = '\0';
+    }
+}
+
+void startNewLine(Line **head) {
+    Line *newLine = malloc(sizeof(Line));
+    newLine->text = malloc(1);
+    newLine->text[0] = '\0';
+    newLine->next = NULL;
+
+    if (*head == NULL) {
+        *head = newLine;
+    } else {
+        Line *current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newLine;
+    }
+}
