a037996cf4a13009f1a7bf3e7d64f64ff9ffe3f5
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-06-19 09:06:44 +0300

    Update main program to use dynamic library
    Updated main.cpp to load the dynamic library caesar.so and use the encrypt and decrypt functions from the library. This includes loading the library with dlopen, finding the functions with dlsym, and handling errors if the functions cannot be found.

--- TheThirdAssignment/main.cpp
+++ TheThirdAssignment/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <dlfcn.h>
+#include <cstring>
 #include "caesar.h"
 
 int main() {
+
+    void* handle = dlopen("./caesar.so", RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "Error: %s\n", dlerror());
+        return 1;
+    }
+
+    char* (*encrypt)(char*, int);
+    encrypt = (char* (*)(char*, int)) dlsym(handle, "encrypt");
+    if (!encrypt) {
+        fprintf(stderr, "Error: %s\n", dlerror());
+        return 1;
+    }
+
+    char* (*decrypt)(char*, int);
+    decrypt = (char* (*)(char*, int)) dlsym(handle, "decrypt");
+    if (!decrypt) {
+        fprintf(stderr, "Error: %s\n", dlerror());
+        return 1;
+    }
+
     char text[] = "Roses are red, violets are blue";
     int key = 1;
 
@@ -13,6 +36,8 @@ int main() {
 
     free(encryptedText);
     free(decryptedText);
+    
+    dlclose(handle);
 
     return 0;
 }
